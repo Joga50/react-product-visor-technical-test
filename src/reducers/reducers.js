3ce3e56c6feb45497b4c648902793f4c8242a7a8
Joga <jose.gabriel.a12@gmail.com>
2023-04-20 17:08:38 -0500

    adding basic css styles to the app

---
+++ src/reducers/reducers.js
@@ -0,0 +1,24 @@
+// reducers.js
+
+export const ADD_PRODUCT = "ADD_PRODUCT";
+export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
+
+export const addProductReducer = (state, action) => {
+  const newProducts = [...state.products, action.payload];
+  localStorage.setItem("products", JSON.stringify(newProducts));
+  return {
+    ...state,
+    products: newProducts,
+  };
+};
+
+export const removeProductReducer = (state, action) => {
+  const updatedProducts = state.products.filter(
+    (p) => p.codigo !== action.payload.codigo
+  );
+  localStorage.setItem("products", JSON.stringify(updatedProducts));
+  return {
+    ...state,
+    products: updatedProducts,
+  };
+};
